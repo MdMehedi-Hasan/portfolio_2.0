d966e0d205490f351a8986e88868354343e89e41
MdMehedi-Hasan <mh19115@gmail.com>
2024-01-11 21:44:31 +0600

    sidebar designed

--- src/Components/Home/SideProfile.js
+++ src/Components/Home/SideProfile.js
@@ -1,24 +1,30 @@
 import React from 'react'
 // import profile from '../../Assets/profile.png'
-import profile from '../../Assets/test.jpg'
-import { FaGithub, FaLinkedinIn, FaStackOverflow } from "react-icons/fa";
+import profile from '../../Assets/profile2.png'
+// import profile from '../../Assets/test.jpg'
+import { FaDev, FaGithub, FaLinkedinIn, FaStackOverflow } from "react-icons/fa";
 import { RiTwitterXFill } from "react-icons/ri";
 import '../../Styles/Sideprofile.css'
 
 export default function SideProfile() {
     /* https://twitter.com/mehedirhasan */
     return (
-        <div className='border border-white rounded-xl p-3 sideProfile'>
-            <div className='w-full h-fit bg-white rounded-lg flex items-end justify-center'>
-                <img src={profile} alt="Profile" className='w-2/4'/>
+        <div className='border border-neutral-500 rounded-xl p-3 sideProfile'>
+            <span className='flex gap-1 text-2xl font-semibold mt-2 mb-5'>Mehedi <sup className='border rounded-full w-5 h-5 text-xs flex items-center justify-center font-normal'>M</sup></span>
+            <div className='w-full h-fit bg-gradient-to-r from-zinc-400 via-gray-300 to-zinc-400 rounded-lg flex items-end justify-center'>
+                <img src={profile} alt="Profile" className='w-2/4' />
             </div>
-            <p></p>
-            <ul className='flex items-center justify-center gap-3 mt-5'>
-                <li className='border border-slate-100 rounded-md p-2 text-slate-100 cursor-pointer'><a href="https://www.linkedin.com/in/mehedi5/" _blank><FaLinkedinIn /></a></li>
-                <li className='border border-slate-100 rounded-md p-2 text-slate-100 cursor-pointer'><a href="https://github.com/MdMehedi-Hasan" _blank><FaGithub /></a></li>
-                <li className='border border-slate-100 rounded-md p-2 text-slate-100 cursor-pointer'><a href="https://stackoverflow.com/users/18447905/mehedi-hasan" _blank><FaStackOverflow /></a></li>
-                <li className='border border-slate-100 rounded-md p-2 text-slate-100 cursor-pointer'><a href="https://twitter.com/mehedirhasan" _blank><RiTwitterXFill /></a></li>
+            <h1 className='px-3 text-lg font-semibold text-center mt-5'>Md Mehedi Hasan <br /> Front-End Developer</h1>
+            <ul className='flex items-center justify-center gap-3 my-5'>
+                <li className='border border-neutral-500 rounded-md p-2 text-slate-100 cursor-pointer'><a href="https://www.linkedin.com/in/mehedi5/" _blank><FaLinkedinIn /></a></li>
+                <li className='border border-neutral-500 rounded-md p-2 text-slate-100 cursor-pointer'><a href="https://github.com/MdMehedi-Hasan" _blank><FaGithub /></a></li>
+                <li className='border border-neutral-500 rounded-md p-2 text-slate-100 cursor-pointer'><a href="https://stackoverflow.com/users/18447905/mehedi-hasan" _blank><FaStackOverflow /></a></li>
+                <li className='border border-neutral-500 rounded-md p-2 text-slate-100 cursor-pointer'><a href="https://twitter.com/mehedirhasan" _blank><RiTwitterXFill /></a></li>
             </ul>
+            <p className='text-xs text-center text-slate-200'>Hello folks, I am Md. Mehedi Hasan a professional front-end developer with more than 2 years of working experience.</p>
+            <div className='flex justify-center mt-5'>
+                <button className='bg-green-600 rounded-full w-2/3 py-2'>Hire Me!</button>
+            </div>
         </div>
     )
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,11 +4,11 @@ import MainContent from '../Components/Home/MainContent'
 
 export default function Home() {
     return (
-        <div className='text-white grid grid-cols-12 gap-5 relative'>
-            <aside className='col-span-4 h-screen sticky top-0'>
+        <div className='text-white grid grid-cols-12 gap-5 relative px-5'>
+            <aside className='col-span-3 h-screen sticky top-0'>
                 <SideProfile />
             </aside>
-            <main className='col-span-8'>
+            <main className='col-span-9'>
                 <MainContent />
             </main>
         </div>
