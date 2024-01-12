a76acbad3a74d4c41da48a84f4fcc0c590bd4c4b
MdMehedi-Hasan <mh19115@gmail.com>
2024-01-12 23:55:08 +0600

    navigation bar added to the right side

--- src/Components/Common/HireModal.js
+++ src/Components/Common/HireModal.js
@@ -7,26 +7,27 @@ const style = {
     transform: 'translate(-50%, -50%)',
     width: 400,
     bgcolor: 'background.paper',
-    border: '2px solid black',
+    border: '2px solid #27d280',
     borderRadius: '20px',
     boxShadow: 24,
     p: 4,
+    textAlign:'center'
 };
-
-export default function HireModal({open,setOpen}) {
+/* Drake #888888 #27d280*/
+export default function HireModal({ open, setOpen }) {
     return (
         <Modal
             open={open}
-            onClose={()=>setOpen(!open)}
+            onClose={() => setOpen(!open)}
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
             <Box sx={style}>
-                <Typography id="modal-modal-title" variant="h6" component="h2">
-                    Text in a modal
+                <Typography id="modal-modal-title" variant="h6" component="h2" sx={{fontWeight:'700'}}>
+                    Eager to Elevate Your Team
                 </Typography>
                 <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                    Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                    I'm excited about the opportunity to bring my skills and enthusiasm to your team. Let's create something amazing together!
                 </Typography>
             </Box>
         </Modal>
--- src/Components/Common/NavMenu.js
+++ src/Components/Common/NavMenu.js
@@ -1,7 +1,23 @@
 import React from 'react'
+import { SlHome } from "react-icons/sl";
+import { GoPerson } from "react-icons/go";
+import { CgMenuRightAlt } from "react-icons/cg";
+import { LiaShapesSolid } from "react-icons/lia";
+import { RxDragHandleDots2 } from "react-icons/rx";
+import { BiMessageDetail } from "react-icons/bi";
+import { MdOutlineMail } from "react-icons/md";
+import { Tooltip } from '@mui/material';
 
 export default function NavMenu() {
   return (
-    <div>NavMenu</div>
+    <ul className='border rounded-3xl w-fit px-2 py-3 flex flex-col gap-3'>
+      <Tooltip title="Home" placement="left" arrow><li className='hover:text-[#27d280] duration-200'><SlHome /></li></Tooltip>
+      <Tooltip title="About" placement="left" arrow><li className='hover:text-[#27d280] duration-200'><GoPerson /></li></Tooltip>
+      <Tooltip title="Services" placement="left" arrow><li className='hover:text-[#27d280] duration-200'><CgMenuRightAlt /></li></Tooltip>
+      <Tooltip title="Skills" placement="left" arrow><li className='hover:text-[#27d280] duration-200'><LiaShapesSolid /></li></Tooltip>
+      <Tooltip title="Portfolios" placement="left" arrow><li className='hover:text-[#27d280] duration-200'><RxDragHandleDots2 /></li></Tooltip>
+      <Tooltip title="Review" placement="left" arrow><li className='hover:text-[#27d280] duration-200'><BiMessageDetail /></li></Tooltip>
+      <Tooltip title="Contact" placement="left" arrow><li className='hover:text-[#27d280] duration-200'><MdOutlineMail /></li></Tooltip>
+    </ul>
   )
 }
