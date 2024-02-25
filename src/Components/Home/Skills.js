3c435cf509d11a6250dc612de2a1390caad08901
MdMehedi-Hasan <mh19115@gmail.com>
2024-02-25 16:17:23 +0600

    mode - skills desction modified

--- src/Components/Home/Skills.js
+++ src/Components/Home/Skills.js
@@ -15,17 +15,16 @@ export default function Skills() {
         { title: 'Tailwindcss', level: '', icon: <SiTailwindcss className='text-6xl' /> },
         { title: 'Material UI', level: '', icon: <SiMui className='text-6xl' /> },
         { title: 'Figma', level: '', icon: <FaFigma className='text-6xl' /> },
-        { title: 'Dev tools', level: '', icon: <BsBrowserChrome className='text-6xl' /> },
         { title: 'Github', level: '', icon: <IoLogoGithub className='text-6xl' /> }
     ]
     return (
         <div>
             <span className='flex items-center gap-2 text-xs border border-[#565656] rounded-full w-fit px-4 py-2'><LiaShapesSolid />SKILLS</span>
             <h1>My Skills</h1>
-            <div className='grid grid-cols-5 gap-y-10'>
+            <div className='grid grid-cols-4 gap-y-10'>
                 {
                     skills.map((item, index) => <div key={index} className='flex flex-col items-center gap-2'>
-                        <span className='border border-red-400 rounded-full py-16 px-10'>{item.icon}</span>
+                        <span className='border border-gray rounded-full py-16 px-10'>{item.icon}</span>
                         {item.title}
                     </div>)
                 }
