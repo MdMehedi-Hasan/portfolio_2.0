130835fdafc3ee135fe6345467242f27c0674ad3
MdMehedi-Hasan <mh19115@gmail.com>
2024-03-17 23:53:59 +0600

    design - ic site images added

--- src/Components/Home/Portfolio.js
+++ src/Components/Home/Portfolio.js
@@ -1,16 +1,25 @@
 import React from 'react'
 import { RxDragHandleDots2 } from 'react-icons/rx'
+import ic1 from '../../Assets/Portfolios/1.png'
+import ic2 from '../../Assets/Portfolios/2.png'
+import ic3 from '../../Assets/Portfolios/3.png'
 
 export default function Portfolio() {
     return (
         <section>
             <span className='flex items-center gap-2 text-xs border border-[#565656] rounded-full w-fit px-4 py-2'><RxDragHandleDots2 />PORTFOLIO</span>
             <div className='grid grid-cols-2 gap-5'>
-                <div className='bg-green-600 h-36 w-full rounded-lg'></div>
-                <div className='bg-red-600 h-36 w-full rounded-lg'></div>
-                <div className='bg-blue-600 h-36 w-full col-span-2 rounded-lg'></div>
-                <div className='bg-purple-600 h-36 w-full rounded-lg'></div>
-                <div className='bg-cyan-600 h-36 w-full rounded-lg'></div>
+                <div className='col-span-2 w-full rounded-lg'>
+                    <img src={ic1} alt="portfolio" className='rounded-lg' />
+                </div>
+                <div className='w-full rounded-lg'>
+                    <img src={ic2} alt="portfolio" className='rounded-lg' />
+                </div>
+                <div className='w-full rounded-lg'>
+                    <img src={ic3} alt="portfolio" className='rounded-lg' />
+                </div>
+                <div className='col-span-2 bg-purple-600 h-36 w-full rounded-lg'></div>
+                <div className='col-span-2 bg-cyan-600 h-36 w-full rounded-lg'></div>
             </div>
         </section>
     )
