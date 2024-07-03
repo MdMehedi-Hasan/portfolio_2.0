cab5a1b6c141a91f23e17a641c51464513e09db5
MdMehedi-Hasan <mh19115@gmail.com>
2024-07-04 01:11:29 +0600

    specialized section designed

--- src/Components/Home/Services.js
+++ src/Components/Home/Services.js
@@ -6,9 +6,9 @@ import { VscDebugAll } from "react-icons/vsc";
 
 export default function Services() {
     const services = [
-        { title: 'Design', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Nostrum ratione aspernatur amet ut eaque eius ducimus.Earum esse sed.', icon: <MdOutlineDesignServices className='text-4xl' /> },
-        { title: 'Development', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Nostrum ratione aspernatur amet ut eaque eius ducimus.Earum esse sed.', icon: <FaCode className='text-4xl' /> },
-        { title: 'Bug Solving', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.Nostrum ratione aspernatur amet ut eaque eius ducimus.Earum esse sed.', icon: <VscDebugAll className='text-4xl' /> }
+        { title: 'Design', description: 'Create visually appealing and user-friendly interfaces for web applications.', icon: <MdOutlineDesignServices className='text-4xl' /> },
+        { title: 'Development', description: 'Build robust and scalable front-end applications.', icon: <FaCode className='text-4xl' /> },
+        { title: 'Bug Solving', description: 'Identify, troubleshoot, and fix issues in web applications.', icon: <VscDebugAll className='text-4xl' /> }
     ]
     return (
         <section>
@@ -17,10 +17,10 @@ export default function Services() {
             <div className='flex flex-col gap-8'>
                 {
                     services.map((item, index) =>
-                        <div className='flex items-start gap-10 p-8 md:p-12 border border-green-300 rounded-3xl' key={index}>
+                        <div className='flex items-start justify-between gap-10 p-8 md:p-12 border border-neutral-500 group hover:border-[#28e98c] transition ease-in-out duration-200 rounded-3xl' key={index}>
                             <div>
-                                <h4 className='text-2xl mb-3'>{item.title}</h4>
-                                <p className=''>{item.description}</p>
+                                <h4 className='text-2xl mb-3 group-hover:text-[#28e98c]'>{item.title}</h4>
+                                <p className='text-sm text-neutral-400'>{item.description}</p>
                             </div>
                             <span className='text-4xl text-green-300'>{item.icon}</span>
                         </div>)
