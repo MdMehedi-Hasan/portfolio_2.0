f753d779eb055cf142f492cf1121860cdb380281
MdMehedi-Hasan <mh19115@gmail.com>
2024-07-04 13:55:49 +0600

    overflow issue solved

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -4,7 +4,7 @@ import MainContent from '../Components/Home/MainContent'
 
 export default function Home() {
     return (
-        <div className='text-white grid grid-cols-12 gap-5 relative px-5'>
+        <div className='text-white grid grid-cols-12 gap-5 relative px-5 overflow-x-hidden'>
             <aside className='col-span-12 lg:col-span-3 h-fit lg:h-screen lg:sticky lg:top-0'>
                 <SideProfile />
             </aside>
